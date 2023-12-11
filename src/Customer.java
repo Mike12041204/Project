64486caa00ceebd34d9f7acb56580e85d1b88a90
Mike12041204 <104843870+Mike12041204@users.noreply.github.com>
2023-12-11 14:41:10 -0500

    All files besides the driver javadocd

--- src/Customer.java
+++ src/Customer.java
@@ -1,5 +1,13 @@
 import Structures.*;
 
+/**
+ * The customer class represents a single customer or a group of customers keyed by a
+ * name. Beyond this, the class has other properties and a list to hold the position
+ * of the groups memebers in a theater.
+ * 
+ * @author Michael Greenbaum and Duwon Ham
+ * @version 12/11/2023
+ */
 public class Customer extends KeyedItem<String>
 {
     // key is name
@@ -10,6 +18,15 @@ public class Customer extends KeyedItem<String>
     
     private List<Integer> positions;
 
+    /**
+     * The constructor for the customer class initializes the variables of the object
+     * by using parameters. It also initializes the empty positions collection.
+     * 
+     * @param groupSize the size of the group
+     * @param name the name and key of the group
+     * @param movie the movie the group is seeing
+     * @param hasChild whether the group contains a child
+     */
     public Customer(int groupSize, String name, String movie, boolean hasChild)
     {
         super(name);
@@ -21,36 +38,72 @@ public class Customer extends KeyedItem<String>
         positions = new List<>();
     }
 
+    /**
+     * Gets the size of customer, the number of people within the group.
+     * 
+     * @return the size of the group
+     */
     public int getSize()
     {
         return size;
     }
 
+    /**
+     * Get the movie the group wants to watch.
+     * 
+     * @return the intended movie
+     */
     public String getMovie()
     {
         return movie;
     }
 
+    /**
+     * Sets the movie the group wants to watch.
+     * 
+     * @param movie that the group will consider
+     */
     public void setMovie(String movie)
     {
         this.movie = movie;
     }
 
+    /**
+     * Gets whether the customer group has a child.
+     * 
+     * @return whether there is a child
+     */
     public boolean getHasChild()
     {
         return hasChild;
     }
 
+    /**
+     * Adds the location of a seat a customer group member is sitting at.
+     * 
+     * @param index position in the array the seat will be added at
+     * @param seat the number of the seat the member is sitting at
+     */
     public void addSeat(int index, int seat)
     {
         positions.add(index, seat);
     }
 
+    /**
+     * Gets a seat a member of the group is sitting at.
+     * 
+     * @param index the postion in the array to get the seat from
+     * @return the seat a member is sitting at
+     */
     public int getSeat(int index)
     {
         return positions.get(index);
     }
 
+    /**
+     * Returns a formatted string displaying the customers group and some relevant
+     * information about them.
+     */
     public String toString()
     {
         return "\tCustomer " + getKey() + " party of " + size + " for " + movie + " movie.\n";
--- src/Line.java
+++ src/Line.java
@@ -1,37 +1,75 @@
 import Structures.*;
 
+/**
+ * The line class represents a line of customers waiting to enter a theater at the
+ * cinema. The line class contains a queue of these people as well as methods to interact
+ * with this structure.
+ * 
+ * @author Michael Greenbaum and Duwon Ham
+ * @version 12/11/2023
+ */
 public class Line {
     private Queue<Customer> train;
     private int size; //We can also make size method in queue class which I think we should, but Im not sure if we can change structure class in this project
 
+    /**
+     * The constructor for the line class initializes the queue and sets the size to
+     * 0.
+     */
     public Line()
     {
         train = new Queue<>();
         size = 0;
     }
 
+    /**
+     * Removes a customer from the line and returns them.
+     * 
+     * @return the customer who left the line
+     */
     public Customer leave()
     {
         size--;
         return train.dequeue();
     }
 
+    /**
+     * Adds a customer to the queue.
+     * 
+     * @param customer the customer who has been added to line
+     */
     public void enter(Customer customer)
     {
         train.enqueue(customer);
         size++;
     }
 
+    /**
+     * Gets whether the line is empty or not.
+     * 
+     * @return whether the line is empty
+     */
     public boolean empty()
     {
         return train.isEmpty();
     }
 
+    /**
+     * Gets the size of the line.
+     * 
+     * @return the number of customers in the line
+     */
     public int size()
     {
         return size;
     }
 
+    /**
+     * Returns a formatted string of the lines contents, including the customers
+     * within it.
+     * 
+     * @return the string representing the line
+     */
     public String toString()
     {
         return train.toString();
